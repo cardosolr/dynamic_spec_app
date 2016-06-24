14d377cf70de46abd77dfb54f02b621c13fc3292
Larissa Cardoso <LarissaRattigueri@Larissas-MacBook-Air.local>
2016-06-24 10:27:30 -0700

    Init Repository

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+
+  def formatDate(date, horario)
+    if horario
+      date.strftime("%b-%d-%Y %I:%M%p")
+    else
+      date.strftime("%b/%d/%Y")
+    end
+  end
+
+end
